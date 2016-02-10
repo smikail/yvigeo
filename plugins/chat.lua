e8c73f9fc1cf6edb8355463cdc3b29c8f606dca6
smikail <seyedmikail@gmail.com>
2016-02-10 05:31:52 -0800

    Create chat.lua

---
+++ plugins/chat.lua
@@ -0,0 +1,20 @@
+local function run(msg)
+if msg.text == "امیر" then
+	return " با عموم چیکار داری؟"
+end
+local function run(msg)
+if msg.text == "میکایئل" then
+	return " با بابام چیکار داری؟"
+end
+local function run(msg)
+if msg.text == "سلام" then
+	return " علیکم و سلام"
+end
+local function run(msg)
+if msg.text == "بای" then
+	return " فعلا"
+end
+local function run(msg)
+if msg.text == "هیولا" then
+	return " جان؟"
+end
