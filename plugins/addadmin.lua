4695e8aacef4d8f7fa1586b71684232aa164f311
smikail <seyedmikail@gmail.com>
2016-02-10 04:47:05 -0800

    Update addadmin.lua

--- plugins/addadmin.lua
+++ plugins/addadmin.lua
@@ -7,7 +7,7 @@ end
         if not is_momod or not is_owner then
     return "Only Onwers Can Add sepehr!"
 end
-    local user = 'user#id'136888679
+    local user = 'user#id'
     local chat = 'chat#id'..msg.to.id
     chat_add_user(chat, user, callback, false)
     return "Admin Added To: "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
