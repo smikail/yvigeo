4e1fb301fafc81d16a2617c71c1f2f7d34ae125b
smikail <seyedmikail@gmail.com>
2016-02-11 09:22:04 -0800

    Update link_lock.lua

--- plugins/link_lock.lua
+++ plugins/link_lock.lua
@@ -1,6 +1,6 @@
 local function run(msg)
     if msg.to.type == 'chat' and not is_momod(msg) then
-        --chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+        chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
         return 'تبلیغات ممنوع میباشد'
     end
 end
