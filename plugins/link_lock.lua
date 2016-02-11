0b48dc2ce39e1f1a05eb96e4162de31150ba80ce
smikail <seyedmikail@gmail.com>
2016-02-11 08:36:08 -0800

    Create link_lock.lua

---
+++ plugins/link_lock.lua
@@ -0,0 +1,16 @@
+local function run(msg)
+    if msg.to.type == 'chat' and not is_momod(msg) then
+        --chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+        return 'تبلیغات ممنوع میباشد'
+    end
+end
+
+return {
+    patterns = {
+    ".com",
+    "http://",
+    "https://",
+    "adf.ly"
+    }, 
+run = run
+}
