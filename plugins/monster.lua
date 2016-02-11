5d069205bc8ae7c87f73bad38160d400a3c7e796
smikail <seyedmikail@gmail.com>
2016-02-11 09:14:00 -0800

    Create monster.lua

---
+++ plugins/monster.lua
@@ -0,0 +1,26 @@
+do
+
+function run(msg, matches)
+  return [[
+بات ورژن1
+بات انتی اسپمر و هوشمند تلگرام
+مدیران ربات
+@Mr_Vigeo [sudo]
+@Xx_minister_salib_xX [sudo]
+تشکر ویژه از
+@Mr_Vigeo 
+چنل ما
+@pluginlua [persian]
+  ]]
+
+  end
+return {
+  description = "shows sudoers", 
+  usage = "!sudoers : return sudousers",
+  patterns = {
+    "^[mM]onster$",
+
+  },
+  run = run
+}
+end
