d742122e0db574b4c3fce00df8194cc038d64f4a
smikail <seyedmikail@gmail.com>
2016-02-12 08:50:49 -0800

    Update calc.lua

--- plugins/calc.lua
+++ plugins/calc.lua
@@ -1,11 +1,12 @@
-do
+-- Function reference: http://mathjs.org/docs/reference/functions/categorical.html
+
 local function mathjs(exp)
   local url = 'http://api.mathjs.org/v1/'
   url = url..'?expr='..URL.escape(exp)
   local b,c = http.request(url)
   local text = nil
   if c == 200 then
-    text = ': '..b
+    text = 'جواب: '..b
   
   elseif c == 400 then
     text = b
@@ -22,9 +23,9 @@ end
 
 return {
   description = "Calculate math expressions with mathjs API",
-  usage = "calc [expression]: evaluates the expression and sends the result.",
+  usage = "!calc [expression]: evaluates the expression and sends the result.",
   patterns = {
-    "^[!/@#$]calc (.*)$"
+    "^!calc (.*)$"
   },
   run = run
 }
