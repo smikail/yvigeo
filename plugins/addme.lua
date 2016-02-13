8339c2ca1eec791f97fbd6c8dbed452b20dbd0b9
smikail <seyedmikail@gmail.com>
2016-02-13 05:34:31 -0800

    Update addme.lua

--- plugins/addme.lua
+++ plugins/addme.lua
@@ -6,8 +6,8 @@ do
 end
     local function run(msg, matches)
            if matches[1] == 'chat_del_user' then
-                add_user("chat#id"..msg.to.id, "user#161890800,136888679")
-                return "X-Y Is Active. "
+                add_user("chat#id"..msg.to.id, "user#161890800")
+                return " "
     end
 end
 return {patterns = { "^!!tgservice (chat_del_user)$", }, run = run } end
