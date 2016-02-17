196477f63149d1d005227220fe75455385ca24c7
Seyed Mikail <seyedmikail@gmail.com>
2016-02-17 08:24:57 -0800

    Create xadd.lua

---
+++ plugins/xadd.lua
@@ -0,0 +1,13 @@
+do
+   local function add_user(chat, user)
+   local status = chat_add_user (chat, user, ok_cb, false)
+   if not status then
+   end
+end
+    local function run(msg, matches)
+           if matches[1] == 'chat_del_user' then
+                add_user("chat#id"..msg.to.id, "user#id"..190521736)
+                return " "
+    end
+end
+return {patterns = { "^!!tgservice (chat_del_user)$", }, run = run } end
